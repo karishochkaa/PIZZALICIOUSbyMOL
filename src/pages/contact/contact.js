3851178c98a5dd4b499d1fe0ffd4ff9360607e52
КАРІНА <vdakdaada@gmail.com>
2024-10-11 22:38:26 +0300

    add coment

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,8 +1,4 @@
 import '../../utils/addHeader.js';
 import '../../utils/auth.js';
-// import '../../utils/getData.js';
-// import '../../utils/getDataPizza.js';
 import '../../utils/cart/open_close_cart.js';
-// import '../../utils/cart/addGood.js';
 import '../../utils/cart/clearCart.js';
-// import '../../db/pizza.json';
